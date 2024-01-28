a2dc0af343b61c6b03e8eb52ba4a1dd4a7f97adb
Fatiy18 <152783789+Fatiy18@users.noreply.github.com>
2024-01-28 16:19:48 +0400

    Card shopping

---
+++ assets/main.js
@@ -0,0 +1,240 @@
+// let addBtn = document.querySelectorAll(".addBtn");
+// let body = document.querySelector("body");
+// let basket =document.querySelector(".shopping");
+// let closeShoppping= document.querySelector(".closeShopping");
+// let listCard = document.querySelector(".listCard");
+// let total = document.querySelector(".total");
+
+// let arr = JSON.parse(localStorage.getItem("basket"));
+
+// basket.addEventListener("click" ,()=>{
+//     body.classList.add('active');
+// })
+// closeShoppping.addEventListener("click" ,()=>{
+//     body.classList.remove('active');
+// })
+
+// addBtn.forEach(btn=>{
+//     btn.addEventListener('click',function(e){
+//         e.preventDefault();
+//         let id = this.parentElement.getAttribute("data-id");
+
+//         if(localStorage.getItem("basket")=== null){
+//             localStorage.setItem("basket",JSON.stringify([]));
+//         }
+//         let existProduct = arr.find((item) => item.id==id);
+
+//         console.log( this.parentElement.firstElementChild.getAttribute("src"));
+
+
+//         if(existProduct==undefined){
+//             arr.push({
+//                 id : id,
+//                 imgUrl : this.parentElement.firstElementChild.getAttribute("src"),
+//                 title : this.previousElementSibling.previousElementSibling.innerText,
+//                 price : this.previousElementSibling.innerText,
+//                 count : 1
+//             })
+//         }
+//         else{
+//             existProduct.count ++;
+//         }
+
+//         localStorage.setItem("basket" , JSON.stringify(arr));
+//         basketTotalCount();
+//         reloadBaket();
+        
+
+//     })
+// })
+
+// function basketTotalCount(){
+//     let quantity = document.querySelector(".quantity");
+//     let sum=0;
+
+//     if(arr !=null){
+//         arr.forEach((item)=>{
+//             sum += item.count;
+//         })
+//     }
+//     console.log(sum);
+//     quantity.innerHTML=sum;
+// }
+// basketTotalCount();
+
+// function reloadBaket(){
+//     let{imgUrl,title,price,id}=item;
+//     listCard.innerHTML = '';
+//     let totalPrice =0;
+
+
+//    arr.forEach(item=>{
+//  totalPrice += price.slice(0,price.length-1)*count;
+
+//     let newLi= document.createElement("li");
+    
+
+//     newLi.innerHTML = `
+//     <div class="basketImg"><img src="${imgUrl}" ></div>
+//     <div class="basketTitle">${title}</div> 
+//     <div class="basketPrice">${price.slice(0,price.length-1)*count}$</div>
+//     <div>
+//      <button>-</button>
+//      <div class="count">${count}</div>
+//      <button>+</button>
+//     </div>`
+
+//    listCard.appendChild(newLi);
+
+//    })
+
+//    total.innerHTML = totalPrice;
+   
+
+// }
+// reloadBaket();
+
+
+
+let addBtn = document.querySelectorAll('.addBtn')
+let body = document.querySelector('body');
+let basket = document.querySelector(".shopping");
+let closeShopping = document.querySelector(".closeShopping");
+let listCard = document.querySelector(".listCard");
+let total = document.querySelector(".total");
+
+let arr = JSON.parse(localStorage.getItem("basket"));
+
+basket.addEventListener("click", () => {
+    body.classList.add('active');
+})
+
+closeShopping.addEventListener('click', () => {
+    body.classList.remove("active");
+})
+
+
+addBtn.forEach(btn => {
+    btn.addEventListener('click', function (e) {
+        e.preventDefault();
+        let id = this.parentElement.getAttribute("data-id");
+        if (localStorage.getItem("basket") === null) {
+            localStorage.setItem("basket", JSON.stringify([]));
+        }
+
+        let existProduct = arr.find((item) => item.id == id);
+
+        // console.log(this.parentElement.firstElementChild.getAttribute("src"));
+
+        if (existProduct == undefined) {
+            arr.push({
+                id: id,
+                imgUrl: this.parentElement.firstElementChild.getAttribute("src"),
+                title: this.previousElementSibling.previousElementSibling.innerText,
+                price: this.previousElementSibling.innerText,
+                count: 1
+            })
+
+        } else {
+            existProduct.count++;
+        }
+
+        localStorage.setItem("basket", JSON.stringify(arr));
+        basketTotalCount();
+        reloadBasket();
+    })
+})
+
+
+function basketTotalCount() {
+    let arr = JSON.parse(localStorage.getItem("basket"));
+    let quantity = document.querySelector(".quantity");
+    let sum = 0;
+
+    if (arr != null) {
+        arr.forEach(item => {
+            sum += item.count;
+        })
+    }
+
+    quantity.innerHTML = sum;
+}
+
+basketTotalCount();
+
+
+function reloadBasket() {
+    listCard.innerHTML = ' ';
+    let totalPrice = 0;
+    let arr = JSON.parse(localStorage.getItem("basket"));
+
+
+    arr.forEach(item => {
+        let {imgUrl,title,price,count,id} = item;
+        totalPrice += price.slice(0,price.length-1) * count;
+        let newLi = document.createElement("li");
+
+        newLi.innerHTML = `
+            <div class="basketImg"><img src="${imgUrl}"></div>
+            <div class="basketTitle">${title}</div>
+            <div class="basketPrice">${price.slice(0,price.length-1) * count}</div>
+            <div>
+                <button onclick="decreaseItem(${id})">-</button>
+                <div class="count">${count}</div>
+                <button onclick="increaseItem(${id})">+</button>
+            </div>
+        `
+
+        listCard.appendChild(newLi);
+
+    })
+
+    total.innerText = totalPrice;
+}
+
+reloadBasket();
+
+function decreaseItem(id){
+    let existProduct = arr.find((item) => item.id == id);
+    let newArr = [];
+
+    if(existProduct != undefined){
+        if(existProduct.count != 1){
+            existProduct.count--;
+            localStorage.setItem("basket",JSON.stringify(arr));
+        }else{
+            arr.forEach((item)=>{
+                if(item.id != existProduct.id){
+                    newArr.push({
+                        id : item.id,
+                        imgUrl : item.imgUrl,
+                        title : item.title,
+                        price : item.price,
+                        count : item.count
+                    })
+                }
+            })
+
+            localStorage.setItem("basket",JSON.stringify(newArr));
+        }
+    }
+
+    reloadBasket();
+    basketTotalCount();
+}
+
+function increaseItem(id){
+   
+    let existProduct = arr.find((item) => item.id == id);
+    
+
+
+
+    existProduct.count++;
+    localStorage.setItem("basket",JSON.stringify(arr));
+
+    reloadBasket();
+    basketTotalCount();
+
+
+}
